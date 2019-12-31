93734f0c265085b0989bccf130d836457eb5a6dc
haoyuan <haoyuanzhou@yahoo.com>
2019-12-31 17:40:37 +0800

    蒙特卡洛求圆周率

--- 作业-蒙特卡罗方法/pi.py
+++ 作业-蒙特卡罗方法/pi.py
@@ -3,10 +3,13 @@
 
 import random
 import datetime
+import time
 random.seed(datetime.datetime.now())
 r = 100.0
 count_in = 0
 count_all = 1000000
+
+start = time.time()
 for i in range(0, count_all):
     x = random.uniform(-100, 100)
     y = random.uniform(-100, 100)
@@ -14,4 +17,5 @@ for i in range(0, count_all):
         count_in += 1
 
 pi = count_in / count_all * 4
-print(pi)
+print("圆周率", pi)
+print("计算花费时间", time.time()-start)
